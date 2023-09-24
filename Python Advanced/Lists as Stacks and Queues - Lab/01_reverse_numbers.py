7f68f26eb8800912ab3d9512158df0bf7fe82f02
Dimitar Dimov <117776955+dimataa1@users.noreply.github.com>
2023-09-24 13:16:48 +0300

    Delete Python Advanced directory

--- Python Advanced/Lists as Stacks and Queues - Lab/01_reverse_numbers.py
+++
@@ -1,7 +0,0 @@
-numbers = input().split()
-stack_nums = []
-
-while numbers:
-    stack_nums.append(numbers.pop())
-
-print(" ".join(stack_nums))
--- Python Advanced/Lists as Stacks and Queues - Lab/02_Matching Parentheses.py
+++
@@ -1,9 +0,0 @@
-s = input()
-stack = []
-for el in range(len(s)):
-    if s[el] == "(":
-        stack.append(el)
-    elif s[el] == ")":
-        
-        last = stack.pop()
-        print(s[last:el + 1])
